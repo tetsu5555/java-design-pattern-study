e7b5ef54da5d11034fb8c681e6a6947cda5e3794
tetsu5555 <tetete1118@gmail.com>
2019-09-16 15:07:43 +0900

    study Adapter (with class)

---
+++ Adapter/PrintBanner.java
@@ -0,0 +1,14 @@
+public class PrintBanner extends Banner implements Print {
+    public PrintBanner(String string) {
+        super(string);
+    }
+    public void printWeak() {
+        showWithParen();
+    }
+    public void printStrong() {
+        showWithAster();
+    }
+    public void printExclamation() {
+        System.out.println(string + "!!");
+    }
+}
