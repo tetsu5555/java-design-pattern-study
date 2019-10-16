6d6fdde98bae7ba290c08e04bcce1439eff9cb4f
tetsu5555 <tetete1118@gmail.com>
2019-10-17 06:51:24 +0900

    Singleton in js

---
+++ InJavascript/Singleton.js
@@ -0,0 +1,51 @@
+// 期待する挙動
+// const earth = new Earth();
+// const earth2 = new Earth();
+
+// earth === earth2 ? console.log("Singleton") : console.log("Not singleton");
+
+// 第一段階
+// インスタンスがパブリックなため書き換えられてしまう
+// function Earth() {
+//      既存のインスタンスが存在すれば返す
+//     if (typeof Earth.instance === "object") {
+//         return Earth.instance;
+//     }
+
+//     this.name = "Earth";
+//     this.age = "4.7+E9";
+
+//     キャッシュする
+//     Earth.instance = this;
+//     return this;
+// }
+
+
+
+
+// 第二段階
+// クロージャーを使用
+function Earth() {
+    // キャッシュされたインスタンス
+    var instance = this;
+
+    Earth = function Earth() {
+        return instance;
+    }
+
+    Earth.prototype = this;
+
+    instance = new Earth();
+
+    instance.constructor = Earth;
+
+    instance.name = "Earth";
+    instance.age = "4.7+E9";
+
+    return instance;
+}
+
+var earth = new Earth();
+console.log(earth.constructor === Earth);
+console.log(earth.constructor);
+console.log(Earth);
